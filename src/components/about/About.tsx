5ec9ac9c95b643da43000ff443c2d5430c7acb48
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-08-27 23:26:13 +0100

    started the about us saection

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,18 +1,34 @@
 import "./style.css"
 import React from 'react'
 import AbouUs from "../../assests/Photo.png"
+import { FaArrowAltCircleRight } from "react-icons/fa"
 
 const About = () => {
   return (
     <div className="aboutContainer">
         <div className="aboutDisplay">
-            {/* <div className="leftAbout">
+            <p className="aboutSubHead">
+                <span>04</span> 
+                ABOUT US
+            </p>
 
-            </div> */}
+            <h1 className="abouthead">
+                42+ years of helping you find the right property.
+            </h1>
 
-            <div className="rightAbout">
-                <img src={AbouUs} alt=""/>
-            </div>
+            <p>Distinctively re-engineer revolutionary meta-services 
+                and premium  At vero eos et accusamus et iusto
+
+                Dignissimos ducimus qui blanditiis praesentium voluptatum 
+                deleniti atque corrupti quos dolores et quas molestias excepturi.Lorem 
+                ipsum dolor sit amet, consectetur adipiscing elit. 
+            </p>
+
+            <button>Learn More <FaArrowAltCircleRight/></button>
+        </div>
+
+        <div className="rightAbout">
+            <img className="aboutImg" src={AbouUs} alt=""/>
         </div>
     </div>
   )
