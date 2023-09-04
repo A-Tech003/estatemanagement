621f243479a5724ca5c5093f2ed1a24ebe514643
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-09-04 22:56:39 +0100

    make some changes in my styling

--- src/components/review/Review.tsx
+++ src/components/review/Review.tsx
@@ -1,12 +1,25 @@
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa"
 import "./style.css"
-import React from 'react';
+import React, { useState } from 'react';
 import Lady from "../../assests/Lady.jpg";
 import Man from "../../assests/Photo.jpg"
 import ManHat from "../../assests/image 15.jpg"
 import Woman from "../../assests/image 16.jpg"
 
+
+
 const Review = () => {
+    const [scrollIndex, setScrollIndex] = useState(0);
+    const scrollLeft = () => {
+        setScrollIndex(Math.max(scrollIndex - 1, 0));
+    };
+
+    const scrollRight = () => {
+        setScrollIndex(Math.min(scrollIndex + 1, 5 - 1));
+    };
+
+
+
   return (
     <div className="reviewContainer">
         <div className="topCon">
@@ -14,12 +27,47 @@ const Review = () => {
                 <h1>What our customers say about us</h1>
             </div>
             <div className="buttons">
-                <div className="arrow"><FaArrowLeft/></div>
-                <div className="arrow"><FaArrowRight/></div>
+                <div onClick={scrollLeft} className="arrow"><FaArrowLeft/></div>
+                <div onClick={scrollRight} className="arrow"><FaArrowRight/></div>
             </div>
         </div>
 
+
         <div className="bottom">
+            <div>
+                <div className="div">
+                    <p>
+                        Distinctively re-engineer revolutionary services and  
+                        <span style={{color: "#A7542A", fontWeight: 800}}> premium  At vero accusamus</span> et iustoDignissimos ducimus 
+                        qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores .
+                    </p>
+                </div>
+                <div className="profile"><img src={Man}/></div>
+            </div>
+            
+            <div>
+                <div className="div">
+                    <p>Completely synergize resource taxing <span style={{color: "#A7542A", fontWeight: 800}}>relationships</span> via premier. Professionally cultivate one-to-one customer service with robust ideas.</p>
+                    <p style={{marginTop: '1rem'}}>Dynamically <span style={{color: "#3B3A5D", fontWeight: 800}}>innovate resource-leveling customer</span> service for state of the art customer service.</p>
+                </div>
+                <div className="profile"><img src={Lady}/></div>
+            </div>
+
+            <div>
+                <div className="div">
+                    <p>Distinctively re-engineer revolutionary services and <span style={{color: "#A7542A", fontWeight: 800}}>premium</span>  At vero accusamus et iustoDignissimos .</p>
+                </div>
+                <div className="profile"><img src={ManHat}/></div>
+            </div>
+
+            <div>
+                <div className="div">
+                    <p>Proactively envisioned multimedia based expertise and <span style={{color: "#A7542A", fontWeight: 800}}>cross-media growth strategies. Seamlessly visualize</span> quality intellectual capital without superior colla idea-sharing. Holistically pontificate installed base portals after maintainable products.</p>
+                    <p style={{marginTop: '1rem'}}>Phosfluorescently engage methodologies with web-enabled technology. </p>
+                </div>
+                <div className="profile"><img src={Woman}/></div>
+            </div>
+
             <div>
                 <div className="div">
                     <p>
