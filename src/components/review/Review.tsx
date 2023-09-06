fe4da4d41ef645d795ab18340e72ecaa646dc3c7
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-09-06 22:45:15 +0100

    i was about to make progress in my customer review scrollable component

--- src/components/review/Review.tsx
+++ src/components/review/Review.tsx
@@ -2,24 +2,16 @@ import { FaArrowLeft, FaArrowRight } from "react-icons/fa"
 import "./style.css"
 import React, { useState, useRef } from 'react';
 import Lady from "../../assests/Lady.jpg";
-import Man from "../../assests/Photo.jpg"
-import ManHat from "../../assests/image 15.jpg"
-import Woman from "../../assests/image 16.jpg"
-
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from 'swiper/react';
-
-// // Import Swiper styles
-// import 'swiper/css';
-// import 'swiper/css/pagination';
-
-// // import required modules
-// import { A11y, Autoplay, Navigation, Pagination } from 'swiper/modules';
+import Man from "../../assests/Photo.jpg";
+import ManHat from "../../assests/image 15.jpg";
+import Woman from "../../assests/image 16.jpg";
+import Slider from "react-slick"
 
 
 
 
-const Review: React.FC = () => {
+
+const Review = () => {
     
     // const [scrollIndex, setScrollIndex] = useState(0);
     // const scrollLeft = () => {
@@ -29,6 +21,16 @@ const Review: React.FC = () => {
     // const scrollRight = () => {
     //     setScrollIndex(Math.min(scrollIndex + 1, 5 - 1));
     // };
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    };
+
+
 
 
 
@@ -44,10 +46,8 @@ const Review: React.FC = () => {
             </div>
         </div>
 
-
-
-        {/* <div className="bottom">
-            <div>
+        <Slider className="bottom" {...settings}>
+          <div>
                 <div className="div">
                     <p>
                         Distinctively re-engineer revolutionary services and  
@@ -80,41 +80,7 @@ const Review: React.FC = () => {
                 </div>
                 <div className="profile"><img src={Woman}/></div>
             </div>
-
-            <div>
-                <div className="div">
-                    <p>
-                        Distinctively re-engineer revolutionary services and  
-                        <span style={{color: "#A7542A", fontWeight: 800}}> premium  At vero accusamus</span> et iustoDignissimos ducimus 
-                        qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores .
-                    </p>
-                </div>
-                <div className="profile"><img src={Man}/></div>
-            </div>
-            
-            <div>
-                <div className="div">
-                    <p>Completely synergize resource taxing <span style={{color: "#A7542A", fontWeight: 800}}>relationships</span> via premier. Professionally cultivate one-to-one customer service with robust ideas.</p>
-                    <p style={{marginTop: '1rem'}}>Dynamically <span style={{color: "#3B3A5D", fontWeight: 800}}>innovate resource-leveling customer</span> service for state of the art customer service.</p>
-                </div>
-                <div className="profile"><img src={Lady}/></div>
-            </div>
-
-            <div>
-                <div className="div">
-                    <p>Distinctively re-engineer revolutionary services and <span style={{color: "#A7542A", fontWeight: 800}}>premium</span>  At vero accusamus et iustoDignissimos .</p>
-                </div>
-                <div className="profile"><img src={ManHat}/></div>
-            </div>
-
-            <div>
-                <div className="div">
-                    <p>Proactively envisioned multimedia based expertise and <span style={{color: "#A7542A", fontWeight: 800}}>cross-media growth strategies. Seamlessly visualize</span> quality intellectual capital without superior colla idea-sharing. Holistically pontificate installed base portals after maintainable products.</p>
-                    <p style={{marginTop: '1rem'}}>Phosfluorescently engage methodologies with web-enabled technology. </p>
-                </div>
-                <div className="profile"><img src={Woman}/></div>
-            </div>
-        </div> */}
+        </Slider>
     </div>
   )
 }
