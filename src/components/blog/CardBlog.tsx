90ec389fdd101af4d58fa2dac88fb651be82a3e6
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-09-12 13:24:59 +0100

    completed the blog section

--- src/components/blog/CardBlog.tsx
+++ src/components/blog/CardBlog.tsx
@@ -5,12 +5,13 @@ import Toppattern from '../../assests/patternTop.png';
 import Pattern from '../../assests/Group 22.png';
 
 import React from 'react'
+import { FaArrowCircleRight } from "react-icons/fa";
 
 const CardBlog = () => {
   return (
     <div className="Blog-Container">
-        <img className='blogPattern blogimg' src={Pattern} alt='background pattern'/>
-        <img className='blogBottomPattern blogimg' src={Toppattern} alt='background pattern'/>
+         <img className='blogTopPattern blogimg' src={Toppattern} alt='background pattern'/>
+        <img className='blogBottomPattern blogimg' src={Pattern} alt='background pattern'/>
         
         <SubHEad
         span= "06"
@@ -19,6 +20,8 @@ const CardBlog = () => {
         />
 
         <Blog/>
+
+        <div className="Blogbutton"><button className="blogBtn">See All Articles <FaArrowCircleRight style={{marginLeft: "0.5rem"}}/></button></div>
     </div>
   )
 }
